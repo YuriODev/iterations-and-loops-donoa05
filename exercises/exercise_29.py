0c414b967ba7b7a5058aabf3e5712c092ef34d03
Antonio <124811794+donoa05@users.noreply.github.com>
2024-03-25 12:14:30 +0000

    Update exercise_29.py

--- exercises/exercise_29.py
+++ exercises/exercise_29.py
@@ -1,1 +1,10 @@
 # Your solution to Exercise 29
+numbers = []
+while True:
+    num = int(input("numbers:"))
+    numbers.append(num)
+    if sum(numbers) == 0:
+        break
+
+sum_squares = sum(num ** 2 for num in numbers)
+print(sum_squares)
