bf53520948a525c7e3fa3cafa333b41ac26120e9
Antonio <124811794+donoa05@users.noreply.github.com>
2024-03-25 12:06:17 +0000

    Update exercise_31.py

--- exercises/exercise_31.py
+++ exercises/exercise_31.py
@@ -1,1 +1,15 @@
 # Your solution to Exercise 31
+days=int(input("Enter the number of days:"))
+temp=[]
+
+for i in range (days):
+  temperature=int(input(f"Enter the temperature for day {i+1}:"))
+  temp.append(temperature)
+temp.sort()
+print(temp[0])
+
+if temp[0]>-18:
+  print("No")
+else:
+  print("Yes")
+  
